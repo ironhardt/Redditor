943c9a082f33ad37aa2592787997873f58f1dc4f
ironhardt <kylehill94@gmail.com>
2022-06-02 23:36:39 -0700

    continued work on scrolling

--- redditor/src/features/PostList.js
+++ redditor/src/features/PostList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import { loadData, selectIsLoading, selectPageAfter } from './postSlice.js';
 import Post from '../components/Post.js';
@@ -11,33 +11,48 @@ export const PostList = () => {
     const postsLoading = useSelector(selectIsLoading)
     const pageAfter = useSelector(selectPageAfter)
     const postTitles = postList.posts.map(post => post.data.title)
-
+    console.log(pageAfter)
 
     const dispatch = useDispatch()
 
+
     let page = {
-        sub: 'javascript',
-        division: 'hot',
+        sub: 'wow',
+        listing: 'hot',
         after: ''
     }
 
-    const onFirstRender = () => {
-        dispatch(loadData(page))
-    }
-    useEffect(onFirstRender, [])
-
+    const scrollRef = useRef(false)
+    const afterRef = useRef(pageAfter)
 
     const handleEndScroll = () => {
         if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-            page.after = pageAfter
+            if (scrollRef.current) return
+            page.after = afterRef.current
+            console.log(page.after)
             dispatch(loadData(page))
+            scrollRef.current = true
+            setTimeout(() => scrollRef.current = false, 500)
         }
     }
 
     useEffect(() => {
+        afterRef.current = pageAfter
+    }, [pageAfter])
+
+
+
+    const executedRef = useRef(false);
+
+    useEffect(() => {
         document.addEventListener('scroll', handleEndScroll)
-        console.log(postTitles.length)
-    }, [postList])
+        if (executedRef.current) return
+        dispatch(loadData(page))
+        executedRef.current = true
+        return () => {
+            document.removeEventListener('scroll', handleEndScroll)
+        }
+    }, [])
 
 
 
--- redditor/src/features/postSlice.js
+++ redditor/src/features/postSlice.js
@@ -4,7 +4,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 export const loadData = createAsyncThunk(
     'postList/loadData',
     async (path, thunkAPI) => {
-        const response = await fetch(`https://www.reddit.com/r/${path.sub}/${path.division}.json${path.after}`)
+        const response = await fetch(`https://www.reddit.com/r/${path.sub}/${path.listing}.json?limit=10&${path.after}`)
         // https://iq.opengenus.org/get-list-of-posts-using-reddit-api/
         const json = await response.json()
         return json
@@ -29,7 +29,7 @@ export const postSlice = createSlice({
             action.payload.data.children.map(post => {
                 state.posts.push(post)
             })
-            state.after = `?after=${action.payload.data.after}`
+            state.after = `after=${action.payload.data.after}`
         },
         [loadData.rejected]: (state, action) => {
 
